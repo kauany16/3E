af95c80ae8461a26d3b95818bf88db8dc14ee08e
kauany <126159556+kauany16@users.noreply.github.com>
2024-03-19 08:05:12 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const botoes = document.querySelectorAll(".botao");
 
-for(let i = 0; i <botoes.length;i++){
-  botoes[i].onclick= function(){
-    for (let J = 0; j< botoes.length; j++){
+for(let i = 0; i <botoes.length; i++){
+  botoes[i].onclick = function(){
+    for (let j = 0; j< botoes.length; j++){
       botoes[j].classlist.remove("ativo");
+
     }
     botoes[i].classlist.add("ativo")
   }
