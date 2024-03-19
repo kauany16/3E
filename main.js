387f4a95bbd0774925b5f2fa64a7c8c61edd41da
kauany <126159556+kauany16@users.noreply.github.com>
2024-03-19 08:03:16 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const botoes = document.querySelectorAll(".botao");
 
-for(let i = 0 <botoes.length;i++){
+for(let i = 0; i <botoes.length;i++){
   botoes[i].onclick= function(){
     for (let J = 0; j< botoes.length; j++){
       botoes[j].classlist.remove("ativo");
