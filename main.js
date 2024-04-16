11bb326b99056d8500e376ed71e68d7d11c2b20e
kauany <126159556+kauany16@users.noreply.github.com>
2024-04-16 08:13:26 -0300

    chfgh

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ for (let i = 0; i<botoes.length; i++) {
     }
 }
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-12-28");
+const tempoObjetivo1 = new Date("2024-12-28T00:00:00");
 let tempoAtual = new Date();
 
 contadore[0].textContent = tempoObjetivo1 - tempoAtual;
