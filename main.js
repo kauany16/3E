52faa9bc8f64501981f46396fb7089e22415b041
kauany <126159556+kauany16@users.noreply.github.com>
2024-04-30 08:14:17 -0300

    aa

--- main.js
+++ main.js
@@ -26,8 +26,8 @@ contadores[0].textContent = calculaTempo(tempoObjetivo1);
 function calculaTempo(tempoObjetivo1){
    let tempoAtual = new Date();
    let tempoFinal = tempoObjetivo1 - tempoAtual;
-   let segundos = Math.floor (tempoFinal - tempoFinal / 1000);
-   let minutos = Math.floor(segunos / 60);
+   let segundos = Math.floor (tempoFinal / 1000);
+   let minutos = Math.floor(segundos/ 60);
    let horas = Math.floor(minutos/ 60);
    let dias = Math.floor(horas / 24);
 
