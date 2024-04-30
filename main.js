4362651ab8d87e94ff5c4a2706208db96e8bee69
kauany <126159556+kauany16@users.noreply.github.com>
2024-04-30 08:04:12 -0300

    aaaa

--- main.js
+++ main.js
@@ -12,7 +12,33 @@ for (let i = 0; i<botoes.length; i++) {
     }
 }
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-12-28T00:00:00");
-let tempoAtual = new Date();
+const tempoObjetivo1 = new Date("2024-12-28T23:59:59");
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+const tempos =[
+    tempoObjetivo1
+]
+
+for(let I=0; I< contadores.length; I++){
+    contadores[I].textContent = calculaTempo(tempos[i]);
+}
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo1){
+   let tempoAtual = new Date();
+   let tempoFinal = tempoObjetivo1 - tempoAtual;
+   let segundos = Math.floor (tempoFinal1 - tempoFinal / 1000);
+   let minutos = Math.florr(segunos / 60);
+   let horas = Math.florr(hora / 60);
+   let dias = Math.florr(horas / 24);
+
+   sugundo %=60;
+   minutos %=60
+   horas %=24;
+
+   return dias + "dias"
+   + horas + "horas"
+   + minutos + "minutos"
+   + segundos + "segundos"
+}
+
+
