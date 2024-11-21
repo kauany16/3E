0f4d2fa4ada226761c694e05b3f2ac5c58053846
kauany <126159556+kauany16@users.noreply.github.com>
2024-11-21 08:45:32 -0300

    21\11\2024

--- main.js
+++ main.js
@@ -1,44 +1,12 @@
-const botoes = document.querySelectorAll(".botao");
-const textos = document.querySelectorAll(".aba-conteudo");
-
-for (let i = 0; i <botoes.length; i++) {
-    botoes [i] .onclick = function(){
-        for(let j= 0; j <botoes.length; j++){
-            botoes[j].classList.remove("ativo");
-            textos[j].classList.remove("ativo");
-        }
-        botoes[i].classList.add("ativo");
-        textos[i].classList.add("ativo");
-    }
-}
-const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-12-28T23:59:59");
-
-const tempos = [
-    tempoObjetivo1
-]
-
-for(let i=0; i< contadores.length; i++){
-    contadores[i].textContent = calculaTempo(tempos[i]);
-}
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
-
-function calculaTempo(tempoObjetivo1){
-   let tempoAtual = new Date();
-   let tempoFinal = tempoObjetivo1 - tempoAtual;
-   let segundos = Math.floor (tempoFinal / 1000);
-   let minutos = Math.floor(segundos/ 60);
-   let horas = Math.floor(minutos/ 60);
-   let dias = Math.floor(horas / 24);
-
-   segundos %= 60;
-   minutos %= 60
-   horas %= 24;
-
-   return dias + "dias"
-   + horas + "horas"
-   + minutos + "minutos"
-   + segundos + "segundos";
+cont getcss = (variavel) =>{
+    const bodyStyles = getComputedStyle(document.body)
+    return bodyStyles.getPropertyPriorityValue(variavel)
 }
 
+const tickConfig = {
+    family: getCSS('--font'),
+    size: 16,
+    color: getCSS('--primary-color')
+}
 
+export {getCSS, tickConfig}
